ffd253b16f69cb56bac7d469c24bb142d95bb17e
AtsiGupta <atsig2@illinois.edu>
2024-09-17 17:47:47 -0500

    width adjustment

--- site-2024-main/layouts/Title/index.js
+++ site-2024-main/layouts/Title/index.js
@@ -1,33 +1,31 @@
-import { Icon } from "@iconify/react"
-import { useState } from "react"
-import AnimatedLogo from "../../components/logo/index.js"
-import Exhibits from "../../components/vv/exhibits"
+import { Icon } from "@iconify/react";
+import AnimatedLogo from "../../components/logo/index.js";
 
 export default function HomeVideo() {
   return (
     <>
-      <div className="flex flex-col pt-24 md:pt-20 justify-between md:h-screen h-[95vh]">
+      <div className="flex flex-col pt-24 md:pt-20 justify-between md:h-screen h-auto min-h-screen">
 
-        <span className="flex flex-col justify-center md:flex-row items-center text-2xl h-full px-4 text-center md:text-left">
-          <div className="flex flex-col md:gap-3">
+        <span className="flex flex-col md:flex-row justify-center items-center text-2xl h-full px-4 text-center md:text-left">
+          <div className="flex flex-col md:gap-3 w-full max-w-full">
             <span className="flex flex-row items-end justify-start">
-              <h1 className="text-3xl md:text-4xl lg:text-5xl font-heading font-bold w-full tracking-wider">
+              <h1 className="text-3xl md:text-4xl lg:text-5xl font-heading font-bold w-full tracking-wider break-words">
                 Engineering Open House
               </h1>
             </span>
-            <h1 className="text-xl md:text-2xl lg:text-2xl w-full tracking-wider font-montserrat mt-2 md:mt-5">
+            <h1 className="text-xl md:text-2xl lg:text-2xl w-full tracking-wider font-montserrat mt-2 md:mt-5 break-words">
               at the <b>University of Illinois Urbana-Champaign</b> brings
             </h1>
-            <h1 className="text-4xl md:text-6xl lg:text-8xl font-montserrat w-full tracking-wider mt-3">
+            <h1 className="text-4xl md:text-6xl lg:text-8xl font-montserrat w-full tracking-wider mt-3 break-words">
               The Age of Exploration
             </h1>
-            <h1 className="font-montserrat lg:text-3xl text-2xl font-bold tracking-wider pt-5 hero-text">
+            <h1 className="font-montserrat lg:text-3xl text-2xl font-bold tracking-wider pt-5 hero-text break-words">
               April 4th & April 5th, 2025
             </h1>
           </div>
 
-          <div className="relative flex flex-col lg:pl-4 py-2">
-            <p className="text-lg font-semibold mb-2 m-4">
+          <div className="relative flex flex-col lg:pl-4 py-2 w-full max-w-full z-20">
+            <p className="text-lg font-semibold mb-2 m-4 break-words">
               Click below for the submission link!
             </p>
             <a href="https://docs.google.com/forms/d/e/1FAIpQLSfb2wrx5jFCLW0A_HZ4mPr_yj-6Vi2i4xSXIq38aLGdOyZxEw/formrestricted" target="_blank" rel="noopener noreferrer">
@@ -35,17 +33,16 @@ export default function HomeVideo() {
                 src="assets/images/2025LogoComp.webp" 
                 alt="2025 Logo" 
                 style={{ borderWidth: '10px' }} 
-                className="border-solid border-black m-4 w-3/4 lg:w-7/8 shadow-2xl shadow-gray-800 items-end"
+                className="border-solid border-black m-4 w-full lg:w-[50%] md:w-[60%] sm:w-[70%] w-[80%] shadow-2xl shadow-gray-800"
               />
             </a>
           </div>
-
         </span>
 
-        <span className="flex flex-row justify-between w-full min-h-[14vh] lg:h-36 z-10">
+        <span className="flex flex-col md:flex-row justify-between w-full min-h-[14vh] lg:h-36 z-30">
           <a
             href="/vv"
-            className="text-left bg-yellow-200 w-1/2 lg:text-4xl md:text-3xl text-2xl flex flex-row items-center font-montserrat gap-3 hover:bg-yellow-400"
+            className="text-left bg-yellow-200 w-full md:w-1/2 lg:text-4xl md:text-3xl text-2xl flex flex-row items-center font-montserrat gap-3 hover:bg-yellow-400 py-3 z-30"
           >
             <h2 className="md:pl-10 pl-2">Exhibits</h2>
             <Icon
@@ -54,7 +51,7 @@ export default function HomeVideo() {
             />
           </a>
           <a
-            className="text-left bg-blue-300 w-1/2 lg:text-4xl md:text-3xl text-2xl flex flex-row items-center font-montserrat gap-3 hover:bg-blue-400"
+            className="text-left bg-blue-300 w-full md:w-1/2 lg:text-4xl md:text-3xl text-2xl flex flex-row items-center font-montserrat gap-3 hover:bg-blue-400 py-3 z-30"
             href="/vv?t=map"
           >
             <h2 className="md:pl-10 pl-2">Campus Map</h2>
@@ -66,5 +63,5 @@ export default function HomeVideo() {
         </span>
       </div>
     </>
-  )
+  );
 }
