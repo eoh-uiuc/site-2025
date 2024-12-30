059dc5cce95db7bf11d6f46b1367c8938815e9f6
Shaan Doshi <shaandoshi4@gmail.com>
2024-12-29 21:24:59 -0600

    allowing google user-agent in middleware

--- site-2024-main/middleware.js
+++ site-2024-main/middleware.js
@@ -1,19 +1,30 @@
-import { authMiddleware } from "@clerk/nextjs"
+import { authMiddleware } from "@clerk/nextjs";
 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: (req) =>
-    !req.url.includes("/admin", "/volunteer", "/api", "/trpc"),
-})
+  // Define public routes using an array instead of a function
+  publicRoutes: [
+    "/",
+    "/contact-us",
+    "/about",
+    // Add all your public routes here
+  ],
+  
+  // Add beforeAuth to handle Googlebot
+  beforeAuth: (req) => {
+    const userAgent = req.headers.get('user-agent') || '';
+    if (userAgent.includes('google')) {
+      return true;
+    }
+    return false;
+  },
+});
 
 export const config = {
   matcher: [
-    "/((?!.+\\.[\\w]+$|_next).*)",
+    "/((?!.*\\.[\\w]+$|_next).*)", // exclude static files
     "/",
     "/(api|trpc)(.*)",
     "/volunteer",
     "/admin",
-  ]
-}
+  ],
+};
