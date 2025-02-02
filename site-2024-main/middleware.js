2678e231f12177097f25b3173c568105dcfde568
Shaan Doshi <shaandoshi4@gmail.com>
2025-02-02 15:20:57 -0600

    change middleware so you can access everything but volunteer and admin

--- site-2024-main/middleware.js
+++ site-2024-main/middleware.js
@@ -2,7 +2,11 @@ import dns from 'dns/promises';
 import { NextResponse } from 'next/server';
 
 async function isGooglebot(req) {
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const ip = req.headers['x-forwarded-for'] || req.connection?.remoteAddress;
+  if (!ip) {
+    console.warn('Invalid IP address');
+    return false;
+  }
   try {
     const hostnames = await dns.reverse(ip);
     return hostnames.some((hostname) => hostname.endsWith('.googlebot.com') || hostname.endsWith('.google.com'));
@@ -12,23 +16,26 @@ async function isGooglebot(req) {
 }
 
 export default async function middleware(req) {
-  const publicRoutes = ['/', '/contact-us', '/about'];
+  // const publicRoutes = ['/', '/contact-us', '/about', '/faq', '/sponsor-us', '/students'];
+  const privateRoutes = ['/volunteer', '/admin']
   const url = req.nextUrl.pathname;
 
-  if (publicRoutes.includes(url)) {
-    return NextResponse.next(); // Allow public routes without auth
-  }
+  // if (publicRoutes.includes(url)) {
+  //   return NextResponse.next(); // Allow public routes without auth
+  // }
 
   if (await isGooglebot(req)) {
     return NextResponse.next(); // Allow Googlebot to access
   }
 
-  // Proceed with your auth logic here
-  const authHeader = req.headers.get('Authorization');
-  if (!authHeader) {
-    const url = req.nextUrl.clone();
-    url.pathname = '/login';
-    return NextResponse.redirect('/login'); // Redirect to login if not authenticated
+  if (privateRoutes.includes(url)) {
+    // Proceed with your auth logic here
+    const authHeader = req.headers.get('Authorization');
+    if (!authHeader) {
+      const url2 = req.nextUrl.clone();
+      url2.pathname = '/login';
+      return NextResponse.rewrite(url2); // Redirect to login if not authenticated
+    }
   }
 
   return NextResponse.next(); // Allow access if authenticated
