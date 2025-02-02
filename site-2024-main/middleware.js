e29caeb15840d9d38d379df6a9ed0f8b0c7667d9
Shaan Doshi <shaandoshi4@gmail.com>
2025-02-02 17:48:32 -0600

    revert middleware

--- site-2024-main/middleware.js
+++ site-2024-main/middleware.js
@@ -1,52 +1,19 @@
-import dns from 'dns/promises';
-import { NextResponse } from 'next/server';
+import { authMiddleware } from "@clerk/nextjs"
 
-async function isGooglebot(req) {
-  const ip = req.headers['x-forwarded-for'] || req.connection?.remoteAddress;
-  if (!ip) {
-    console.warn('Invalid IP address');
-    return false;
-  }
-  try {
-    const hostnames = await dns.reverse(ip);
-    return hostnames.some((hostname) => hostname.endsWith('.googlebot.com') || hostname.endsWith('.google.com'));
-  } catch (err) {
-    return false; // If DNS fails, assume it's not a Googlebot
-  }
-}
-
-export default async function middleware(req) {
-  // const publicRoutes = ['/', '/contact-us', '/about', '/faq', '/sponsor-us', '/students'];
-  const privateRoutes = ['/volunteer', '/admin']
-  const url = req.nextUrl.pathname;
-
-  // if (publicRoutes.includes(url)) {
-  //   return NextResponse.next(); // Allow public routes without auth
-  // }
-
-  if (await isGooglebot(req)) {
-    return NextResponse.next(); // Allow Googlebot to access
-  }
-
-  if (privateRoutes.includes(url)) {
-    // Proceed with your auth logic here
-    const authHeader = req.headers.get('Authorization');
-    if (!authHeader) {
-      const url2 = req.nextUrl.clone();
-      url2.pathname = '/login';
-      return NextResponse.rewrite(url2); // Redirect to login if not authenticated
-    }
-  }
-
-  return NextResponse.next(); // Allow access if authenticated
-}
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
+export default authMiddleware({
+  publicRoutes: (req) =>
+    !req.url.includes("/admin", "/volunteer", "/api", "/trpc"),
+})
 
 export const config = {
   matcher: [
-    '/((?!.*\\..*|_next).*)', // Exclude static files and _next folder
-    '/',
-    '/api/trpc/(.*)',
-    '/volunteer',
-    '/admin',
-  ],
-};
+    "/((?!.+\\.[\\w]+$|_next).*)",
+    "/",
+    "/(api|trpc)(.*)",
+    "/volunteer",
+    "/admin",
+  ]
+}
