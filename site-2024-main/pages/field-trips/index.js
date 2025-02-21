18795a7e0e95a95dfa28be5ef64b05f685512c20
Shaan Doshi <shaandoshi4@gmail.com>
2025-02-20 19:00:20 -0600

    atsi's changes and some field trip

--- site-2024-main/pages/field-trips/index.js
+++ site-2024-main/pages/field-trips/index.js
@@ -3,26 +3,37 @@ import Button from '@/button';
 
 export default function FieldTripsPage() {
 	return (
-		<Content title="Field Trip Registration Form">
+		<Content title="Field Trips">
 			<div className="lg:flex">
 				<img className="w-48 h-48 mx-auto lg:mr-10 mb-10" src="assets/background/children.png" alt="" />
 				<div className="lg:col-span-3 mb-10">
 					<p className="text-center font-montserrat">
-						As an entirely student run event, EOH is passionate about empowering and encouraging the next generation of students to pursue a STEM related field. This is why we are beyond thrilled to see your students on a field-trip for the event!
-						<br/><br/>
-						Our registration form is now open, register your field-trip for EOH with the form below! The deadline to register is <span  className="text-theme-red font-bold">March 30th</span>. Information about bus drop-off locations, field-trip schedules, and the visitors guide will all be updated here and emailed to those who register as soon as we have all of those details.
+					This year we are asking for your estimated time of arrival to campus to help us plan traffic management. Please fill out this short form to let us know you're coming: 
 					</p>
+					<Button
+					type="big"
+					className="px-4 rounded-none drop-shadow-md mt-10"
+					href="https://docs.google.com/forms/d/e/1FAIpQLSfywsbT6PwecdAalmL3_B7ceadtniTmXVi5J4ae4qLSP6LxGg/viewform?usp=sf_link"
+					target="_blank"
+					>
+						Registration Form
+					</Button>
 				</div>
 			</div>
 			<div className="mx-auto text-center">
-				<Button
+				<h1 className='title'>MSDC and HSDC</h1>
+				<p className="text-center font-montserrat">Our Middle School and High School Design Competitions are engineering-focused competitions on Friday, April 4.  
+					Students work in teams of 3-5 to create a design and presentation that will be evaluated at the end of the competition. It's a great opportunity for students to 
+					get hands-on engineering design and teamwork experience! SOMESPACE Register your teams here: MSDC: 
+</p>
+				{/* <Button
 					type="big"
 					className="px-4 rounded-none drop-shadow-md"
 					href="https://docs.google.com/forms/d/e/1FAIpQLSfywsbT6PwecdAalmL3_B7ceadtniTmXVi5J4ae4qLSP6LxGg/viewform?usp=sf_link"
 					target="_blank"
 				>
 					Registration Form
-				</Button>
+				</Button> */}
 			</div>
 		</Content>
 	);
