93d5ce7a9a3e7b74ce8202a66d11e480b371db48
AtsiGupta <atsig2@illinois.edu>
2025-03-31 01:56:04 -0500

    footer gradient

--- site-2024-main/components/footer/index.js
+++ site-2024-main/components/footer/index.js
@@ -1,136 +1,86 @@
 import Link from 'next/link';
 
 const footerSections = [
-	// { // TODO: Move Linktree to this section
-	// 	heading: "Visitors",
-	// 	links: [
-	// 		{
-	// 			text: "Visitor Guide",
-	// 			href: "/guide"
-	// 		},
-	// 		{
-	// 			text: "Visitor View",
-	// 			href: "/visit"
-	// 		},
-	// 		{
-	// 			text: "Schedule",
-	// 			href: "/#schedule"
-	// 		}, 
-	// 	]
-	// },
-	{
-		heading: "Special Events",
-		links: [
-			{
-				text: "Middle School Design Competition",
-				href: "/msdc"
-			},
-			{
-				text: "High School Design Competition",
-				href: "/hsdc"
-			},
-		]
-	},
-	{
-		heading: "Get Involved",
-		links: [
-			{
-				text: "Linktree",
-				href: "https://linktr.ee/eoh2025"
-			},
-			{
-				//text: "Volunteer Interest",
-				//href: "https://docs.google.com/forms/d/e/1FAIpQLSfXMPKqUqPbuXvEY6CmlR8lLbFqCM7IYjls1iAi6unnfy-YDQ/viewform?usp=sf_link"
-			},
-			{
-				//text: "Judging Interest",
-				//href: "https://forms.gle/3HXs2EHvqt87nQM19"
-			},
-		]
-	},
-	{
-		heading: "EOH Committee",
-		links: [
-			{
-				text: "Contact Us",
-				href: "/contact-us"
-			},
-			{
-				text: "Support EOH",
-				href: "/#sponsors"
-			},
-			{
-				text: "Engineering Council",
-				href: "https://www.ecillinois.org/"
-			},
-			{
-				text: "History of EOH",
-				href: "https://grainger.illinois.edu/news/features/EOH-100"
-			},
-		]
-	},
-	{
-		heading: "Keep In Touch",
-		links: [
-			{
-				text: "Mailing List",
-				href: "https://forms.gle/A7QqftMGGXKgbBmv5"
-			},
-			{
-				text: "Facebook",
-				href: "https://www.facebook.com/EngineeringOpenHouse/"
-			},
-			{
-				text: "Instagram",
-				href: "https://www.instagram.com/eoh_illinois/"
-			},
-			{
-				text: "X (formerly Twitter)",
-				href: "https://twitter.com/IllinoisEOH"
-			} 
-		]
-	},
-]
+  {
+    heading: "Special Events",
+    links: [
+      { text: "Middle School Design Competition", href: "/msdc" },
+      { text: "High School Design Competition", href: "/hsdc" },
+    ],
+  },
+  {
+    heading: "Get Involved",
+    links: [
+      { text: "Linktree", href: "https://linktr.ee/eoh2025" },
+    ],
+  },
+  {
+    heading: "EOH Committee",
+    links: [
+      { text: "Contact Us", href: "/contact-us" },
+      { text: "Support EOH", href: "/#sponsors" },
+      { text: "Engineering Council", href: "https://www.ecillinois.org/" },
+      { text: "History of EOH", href: "https://grainger.illinois.edu/news/features/EOH-100" },
+    ],
+  },
+  {
+    heading: "Keep In Touch",
+    links: [
+      { text: "Mailing List", href: "https://forms.gle/A7QqftMGGXKgbBmv5" },
+      { text: "Facebook", href: "https://www.facebook.com/EngineeringOpenHouse/" },
+      { text: "Instagram", href: "https://www.instagram.com/eoh_illinois/" },
+      { text: "X (formerly Twitter)", href: "https://twitter.com/IllinoisEOH" },
+    ],
+  },
+];
 
 export default function Footer({ socials }) {
-	return (
-		<footer
-			className="flex flex-col w-full p-3 md:p-12 bg-no-repeat bg-bottom bg-cover"
-			style={{ backgroundImage: "url('/assets/background/website-wide.png')" }} // Path to your PNG file
-		>
-			{/* Translucent Overlay */}
-			{/* <div
-			className="absolute inset-0 bg-black opacity-50 z-0"
-			style={{
-			pointerEvents: "none",
-			top: 0, // Constrain overlay to specific area
-			bottom: "auto", // Prevent overlay from covering non-background areas
-			height: "100%", // Overlay height matching the background section
-			}}
-		></div> */}
-			<div className="flex flex-col md:flex-row w-full items-center md:items-start">
-				{/* <div className="">
-					<img src="/android-chrome-512x512.png" alt="Alma Mater" className='h-60 lg:h-72 w-60 lg:w-72 min-w-min md:mr-12' />
-				</div> */}
-				
-				<div className="flex flex-row flex-wrap items-stretch justify-evenly md:w-2/3">
-					{
-						footerSections.map(section => {
-							return (
-								<div className="flex flex-col p-2 w-44 gap-1" key={section.heading}>
-									<h3 className='font-semibold mb-1 font-montserrat text-gray-600'>{section.heading}</h3>
-									{section.links?.map(link => {
-										return (
-											<a href={link.href} className="hover:text-blue-700 duration-200 font-montserrat text-gray-700" key={link.href}>{link.text}</a>
-										)
-									})}
-								</div>
-							)
-						})
-					}
-				</div>
-			</div>
-			<p className="font-montserrat text-sm text-gray-600 text-center">Copyright EOH 2025</p>
-		</footer>
-	);
+  return (
+    <footer
+      className="relative flex flex-col w-full p-3 md:p-12 bg-no-repeat bg-bottom bg-cover"
+      style={{ backgroundImage: "url('/assets/background/website-wide.png')" }}
+    >
+      {/* Translucent Overlay (not in use, preserved) */}
+      {/* <div
+        className="absolute inset-0 bg-black opacity-50 z-0"
+        style={{
+          pointerEvents: "none",
+          top: 0,
+          bottom: "auto",
+          height: "100%",
+        }}
+      ></div> */}
+
+      {/* Purple-to-transparent gradient behind text */}
+      <div
+        className="absolute top-0 left-0 w-full h-full z-0 pointer-events-none"
+        style={{
+          background: "linear-gradient(to top, rgba(128, 0, 128, 0.65) 25%, rgba(128, 0, 128, 0) 85%)",
+        }}
+      />
+
+      <div className="relative z-10 flex flex-col md:flex-row w-full items-center md:items-start">
+        <div className="flex flex-row flex-wrap items-stretch justify-evenly md:w-2/3">
+          {footerSections.map((section) => (
+            <div className="flex flex-col p-2 w-44 gap-1" key={section.heading}>
+              <h3 className="font-bold mb-1 font-montserrat text-black">{section.heading}</h3>
+              {section.links?.map((link) => (
+                <a
+                  href={link.href}
+                  className="hover:text-blue-700 duration-200 font-montserrat font-medium text-black"
+                  key={link.href}
+                >
+                  {link.text}
+                </a>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <p className="relative z-10 font-montserrat text-sm text-black text-center mt-4 font-medium">
+        Copyright EOH 2025
+      </p>
+    </footer>
+  );
 }
